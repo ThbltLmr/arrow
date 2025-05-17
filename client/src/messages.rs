7c46704338d18e854c346afd2c3b6db76909b9e1
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-05-18 00:48:45 +0200

    feat(messages): impl from traits

--- client/src/messages.rs
+++ client/src/messages.rs
@@ -41,3 +41,39 @@ impl Posture {
         }
     }
 }
+
+impl From<String> for Posture {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "SHOULDERS_NOT_VISIBLE" => Posture::ShouldersNotVisible,
+            "HEAD_NOT_VISIBLE" => Posture::HeadNotVisible,
+            "SLOUCHING_BACK" => Posture::SlouchingBack,
+            "LEANING_IN" => Posture::LeaningIn,
+            "HEAD_TILT_LEFT" => Posture::HeadTiltLeft,
+            "HEAD_TILT_RIGHT" => Posture::HeadTiltRight,
+            "BODY_TILT_LEFT" => Posture::BodyTiltLeft,
+            "BODY_TILT_RIGHT" => Posture::BodyTiltRight,
+            "STRAIGHT" => Posture::Straight,
+            "UNKNOWN" => Posture::Unknown,
+            _ => Posture::Unknown,
+        }
+    }
+}
+
+impl From<&str> for Posture {
+    fn from(value: &str) -> Self {
+        match value {
+            "SHOULDERS_NOT_VISIBLE" => Posture::ShouldersNotVisible,
+            "HEAD_NOT_VISIBLE" => Posture::HeadNotVisible,
+            "SLOUCHING_BACK" => Posture::SlouchingBack,
+            "LEANING_IN" => Posture::LeaningIn,
+            "HEAD_TILT_LEFT" => Posture::HeadTiltLeft,
+            "HEAD_TILT_RIGHT" => Posture::HeadTiltRight,
+            "BODY_TILT_LEFT" => Posture::BodyTiltLeft,
+            "BODY_TILT_RIGHT" => Posture::BodyTiltRight,
+            "STRAIGHT" => Posture::Straight,
+            "UNKNOWN" => Posture::Unknown,
+            _ => Posture::Unknown,
+        }
+    }
+}
